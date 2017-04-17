94122a9b61d6d9c63ec365e645b5c2e12f62e89d
Jannik Bikowski <janbiko@gmx.de>
2017-04-17 12:37:08 +0200

    Fixed lag caused by shooting sound

--- Code/mediaplayer.cpp
+++ Code/mediaplayer.cpp
@@ -6,33 +6,35 @@ MediaPlayer::MediaPlayer() : QObject()
     mpBGMusicPlaylist = new QMediaPlaylist();
     mpBGMusicPlaylist->addMedia(QUrl("../assets/snd/background_music.wav"));
     mpBGMusicPlaylist->setPlaybackMode(QMediaPlaylist::Loop);
+    mpBGMusic = new QMediaPlayer();
+    mpBGMusic->setPlaylist(mpBGMusicPlaylist);
 
     // shooting sound
     mpShootingSoundPlaylist = new QMediaPlaylist();
     mpShootingSoundPlaylist->addMedia(QUrl("../assets/snd/shoot.mp3"));
+    mpShootingSound = new QMediaPlayer();
+    mpShootingSound->setPlaylist(mpShootingSoundPlaylist);
 
     // reload sound
     mpReloadSoundPlaylist = new QMediaPlaylist();
     mpReloadSoundPlaylist->addMedia(QUrl("../assets/snd/reload.mp3"));
+    mpReloadSound = new QMediaPlayer();
+    mpReloadSound->setPlaylist(mpReloadSoundPlaylist);
 }
 
 void MediaPlayer::playBackgroundMusic()
-{
-    mpBGMusic = new QMediaPlayer();
-    mpBGMusic->setPlaylist(mpBGMusicPlaylist);
+{    
     mpBGMusic->play();
 }
 
 void MediaPlayer::playShootingSound()
 {
-    mpShootingSound = new QMediaPlayer();
-    mpShootingSound->setPlaylist(mpShootingSoundPlaylist);
+    mpShootingSound->stop();
     mpShootingSound->play();
 }
 
 void MediaPlayer::playReloadSound()
 {
-    mpReloadSound = new QMediaPlayer();
-    mpReloadSound->setPlaylist(mpReloadSoundPlaylist);
+    mpReloadSound->stop();
     mpReloadSound->play();
 }
